0e0ed7c1ae7ebc81899966e96efe3fa63247be4f
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-10-30 13:48:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,11 +5,6 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
